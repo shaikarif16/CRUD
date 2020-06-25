554236ae2116e014e508ccb0d9ffeb25766a94ab
Arif Shaik <shaikarif00207@gmal.com>
2020-06-25 16:04:49 +0530

    Updates

---
+++ precache-manifest.c5bc352fa4ef3d6ed76fa6011ccc083f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11b9f0a322ac22467a1178da70157f6",
+    "url": "/React---Todo-Lists/index.html"
+  },
+  {
+    "revision": "88e657256a4df2620221",
+    "url": "/React---Todo-Lists/static/css/main.e917c0a8.chunk.css"
+  },
+  {
+    "revision": "b2d85166c45a83a5455f",
+    "url": "/React---Todo-Lists/static/js/2.4bff59cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React---Todo-Lists/static/js/2.4bff59cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88e657256a4df2620221",
+    "url": "/React---Todo-Lists/static/js/main.fc66dfd3.chunk.js"
+  },
+  {
+    "revision": "4859dc4e45dd8dbccad5",
+    "url": "/React---Todo-Lists/static/js/runtime-main.8d70a60b.js"
+  }
+]);
